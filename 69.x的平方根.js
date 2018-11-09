ef9451fffc18809cb0b46da667641807110dbf3d
wangjinxu <1576520768@qq.com>
2018-11-09 17:55:27 +0800

    feat(add) p69 success

--- 69.x的平方根.js
+++ 69.x的平方根.js
@@ -2,10 +2,25 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-  let left = 1;
-  let right = x;
-  let mid = parseInt(x / 2);
-
-  
+var mySqrt = function (x) {
+  if(x === 0 || x === 1) {
+    return x;
+  }
+  left = 0;
+  right = parseInt(x / 2);
+  do {
+    mid = parseInt(left + ((right - left) / 2));
+    if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
+      return mid;
+    } else if (mid * mid > x) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  } while (left <= right);
 };
+for (let i = 0; i < 100; i++) {
+  console.log(mySqrt(i) + '  ' + i);
+}
+
+// mySqrt(9);
