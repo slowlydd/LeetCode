c24a6318ec7e00fbdac4fccf27cc359383f8f4b1
wsWangjinxu <1576520768@qq.com>
2020-10-09 23:17:08 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,18 @@
 module.exports = {
   themeConfig: {
     logo: '/favicon-32x32.cca5dc2d.png',
+    // 导航
     nav: [
-      { text: '所有题解', link: '/' },
-      { text: '每日一题', link: '/' },
-      { text: '周赛题解', link: '/guide/' },
-      { text: '学习资源', link: '/guide/' },
+      { text: 'home', link: '/' },
+      { text: '专题分类', link: '/sort/' },
+      { text: '每日一题', link: '/dailyQuestion/' },
+      { text: '周赛题解', link: '/weeklyContest/' },
+      { text: '顺序题解', link: '/orderBy/'},
+      { text: '学习资源', link: '/resource/' },
       { text: 'Github', link: 'https://github.com/wsWangjinxu/LeetCode' },
+    ],
+    sidebar: [
+      ['/dailyQuestion/202010/', "2020年10月"]
     ]
   },
   head: [
