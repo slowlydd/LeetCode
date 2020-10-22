12d54e321f5b8ddeb3fb3fb08a6c149a0943b899
wsWangjinxu <1576520768@qq.com>
2020-10-23 06:12:25 +0800

    daily question

---
+++ all/234. 回文链表.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    if(!head || !head.next) return true
+    // 快慢指针找中点
+    let f= head, s = head 
+    do {
+        f = f.next.next
+        s = s.next
+    } while(f && f.next)
+
+    let pre = null
+    let curr = s.next
+    if(!curr) return false
+    while(curr) {
+        let temp = curr.next
+        curr.next = pre
+        pre = curr
+        curr = temp
+    }
+    let p = head
+    while(pre) {
+        if(p.val != pre.val) {
+            return false
+        }
+    }
+    return true    
+};
