2a8d116ad3b5ec9f1d5512c1aec0d7270aa9046e
wsWangjinxu <1576520768@qq.com>
2020-10-23 00:59:09 +0800

    daily question

---
+++ all/763. 划分字母区间.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var partitionLabels = function(S) {
+    const last = new Array(26)
+    const length = S.length
+    const codePointA = 'a'.codePointAt(0)
+    for(let i = 0; i < length; i++) {
+        last[S.codePointAt[i] - codePointA] = i
+    }
+
+    const partition = []
+    let start = 0, end = 0
+    for(let i = 0; i < length; i++) {
+        end = Math.max(end, last[S.codePointAt(i) - codePointA]);
+        if(i == end) {
+            partition.push(end - start + 1)
+            start = end + 1
+        }
+    }
+    return partition
+};
