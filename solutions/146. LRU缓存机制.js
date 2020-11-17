33396e6bb9c8542345eb8bc5cd6d9858e57cb3f3
wsWangjinxu <1576520768@qq.com>
2020-11-17 10:09:46 +0800

    daily question

--- solutions/146. LRU缓存机制.js
+++ solutions/146. LRU缓存机制.js
@@ -1,10 +1,41 @@
+function ListNode(key, val) {
+    this.key = key
+    this.val = val
+    this.pre = this.next = null
+}
+
+LRUCache.prototype.removeNode = function (node) {
+    let preNode = node.pre
+    nextNode = node.next
+    preNode.next = nextNode
+    nextNode.pre = preNode
+}
+
+LRUCache.prototype.appendHead = function (node) {
+    let firstNode = this.head.next
+    this.head.next = node
+    node.pre = this.head
+    node.next = firstNode
+    firstNode.pre = node
+}
+
+LRUCache.prototype.removeTail = function() {
+    let key = this.tail.pre.key
+    this.removeNode(this.tail.pre)
+    return key
+}
+
 /**
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
     this.capacity = capacity
-    this.map = new Map() // map 存储映射
-    this.list = []
+    this.size = 0
+    this.data = {}
+    this.head = new ListNode()
+    this.tail = new ListNode()
+    this.head.next = this.tail
+    this.tail.pre = this.head
 };
 
 /** 
@@ -12,8 +43,11 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if(this.map.has(key)) {
-        return this.map.get(key)
+    if(this.data[key] !== undefined) {
+        let node = this.data[key]
+        this.removeNode(node)
+        this.appendHead(node)
+        return node.val
     } else {
         return -1
     }
@@ -25,16 +59,22 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    // 容量达到上限
-    if(this.list.length === this.capacity) {
-        // 删除末尾的 key
-        this.map.delete(this.list[this.capacity - 1])
-        this.list.unshift(key)
-        this.map.set(key, value)
+    let node
+    if(this.data[key] !== undefined) {
+        node = this.data[key]
+        this.removeNode(node)
+        node.val = value
     } else {
-        this.list.unshift(key)
-        this.map.set(key, value)
+        node = new ListNode(key, value) 
+        this.data[key] = node
+        if(this.size < this.capacity) {
+            this.size++
+        } else {
+            key = this.removeTail()
+            delete this.data[key]
+        }
     }
+    this.appendHead(node)
 };
 
 /**
@@ -43,14 +83,3 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
-
- let obj = new LRUCache(2)
- obj.put(1, 1)
- obj.put(2, 2)
- obj.get(1)
- obj.put(3, 3)
- obj.get(2)
- obj.put(4, 4)
- obj.get(1)
- obj.get(3)
- obj.get(4)
